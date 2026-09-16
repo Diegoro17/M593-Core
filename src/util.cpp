f46f81179c97f05d66dfc14b94539017e50b2d89
Diegoro17 <dalejandroordonez@hotmail.com>
2026-09-16 13:54:42 -0500

    fix: use compatible wallet version and isolated M593 data directory

--- src/util.cpp
+++ src/util.cpp
@@ -88,8 +88,8 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char *const RAVEN_CONF_FILENAME = "raven.conf";
-const char *const RAVEN_PID_FILENAME = "ravend.pid";
+const char *const RAVEN_CONF_FILENAME = "m593.conf";
+const char *const RAVEN_PID_FILENAME = "m593d.pid";
 
 ArgsManager gArgs;
 bool fPrintToConsole = false;
@@ -551,11 +551,11 @@ fs::path GetDefaultDataDir()
 {
     // Windows < Vista: C:\Documents and Settings\Username\Application Data\Raven
     // Windows >= Vista: C:\Users\Username\AppData\Roaming\Raven
-    // Mac: ~/Library/Application Support/Raven
-    // Unix: ~/.raven
+    // Mac: ~/Library/Application Support/M593
+    // Unix: ~/.m593
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Raven";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "M593";
 #else
     fs::path pathRet;
     char *pszHome = getenv("HOME");
@@ -565,10 +565,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Raven";
+    return pathRet / "Library/Application Support/M593";
 #else
     // Unix
-    return pathRet / ".raven";
+    return pathRet / ".m593";
 #endif
 #endif
 }

a6c70c18043aaa5092228fa1903b318650988b77
Diegoro17 <dalejandroordonez@hotmail.com>
2026-09-19 12:33:33 -0500

    build: use archived qrencode release tarball

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -1,16 +1,12 @@
 package=qrencode
 $(package)_version=3.4.4
-$(package)_download_path=https://github.com/fukuchi/libqrencode/archive/refs/tags
-$(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=ab7cdf84e3707573a39e116ebd33faa513b306201941df3f5e691568368d87bf
+$(package)_download_path=https://archive.debian.org/debian/pool/main/q/qrencode
+$(package)_file_name=$(package)_$($(package)_version).orig.tar.bz2
+$(package)_sha256_hash=efe5188b1ddbcbf98763b819b146be6a90481aac30cfc8d858ab78a19cde1fa5
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared -without-tools --disable-sdltest
 $(package)_config_opts_linux=--with-pic
-endef
-
-define $(package)_preprocess_cmds
-  mkdir -p m4 && ./autogen.sh
 endef
 
 define $(package)_config_cmds
